e5e661dd282d3b4f30a5f22842db04f360e4ff25
Sergio Mora <sergio.mora.12@sansano.usm.cl>
2017-08-28 19:51:12 -0300

    Update lomitoAleman.c

--- ProyectoLomitoAleman/lomitoAleman.c
+++ ProyectoLomitoAleman/lomitoAleman.c
@@ -153,3 +153,86 @@ struct Promocion * crearPromocion (int codigoIngresado, int precioIngresado){
 	
 	return nuevo;
 }
+
+struct Producto *modificarProducto(int codigoIngresado, char *nombreIngresado, int precioIngresado)
+{
+	struct Producto *modificaciones;
+	int largo;
+
+	modificaciones=((struct Producto*)malloc(sizeof(struct Producto)));
+
+	largo=strlen(nombreIngresado);
+	modificaciones->nombre=(char*)malloc(largo*sizeof(char));
+	strcpy(modificaciones->nombre,nombreIngresado);
+
+	modificaciones->codigo=codigoIngresado;
+	modificaciones->precio=precioIngresado;
+
+	return modificaciones;
+}
+
+struct Sucursal *modificarSucursal(char *direccionIngresada, char *administradorIngresada, char *rutAdministradorIngresada)
+{
+	struct Sucursal *modificaciones;
+	int largo;
+
+	modificaciones=((struct Sucursal*)malloc(sizeof(struct Sucursal)));
+
+	largo=strlen(direccionIngresada);
+	modificaciones->direccion=(char*)malloc(largo*sizeof(char));
+	strcpy(modificaciones->direccion,direccionIngresada);
+
+	largo=strlen(administradorIngresada);
+	modificaciones->administrador=(char*)malloc(largo*sizeof(char));
+	strcpy(modificaciones->administrador,administradorIngresada);
+
+	largo=strlen(rutAdministradorIngresada);
+	modificaciones->rutAdministrador=(char*)malloc(largo*sizeof(char));
+	strcpy(modificaciones->rutAdministrador,rutAdministradorIngresada);
+
+
+	return modificaciones;
+}
+
+struct LomitoAleman *modificarDueno(char *nombreIngresado, struct Sucursal *sucursalesActuales)
+{
+	struct LomitoAleman *modificaciones;
+	int largo;
+
+	largo=strlen(nombreIngresado);
+	modificaciones->dueno=(char*)malloc(largo*sizeof(char));
+	strcpy(modificaciones->dueno,nombreIngresado);
+
+	modificaciones->sucursales=sucursalesActuales;
+
+	return modificaciones;
+}
+
+
+
+
+
+char *ingresarChar(char *variable){ // devuelve un char que se ingresa
+									// ejmplo, ingrese el nombre de "dueno"   o ingrese el nombre de "un usuario"
+	char *nombre;
+	char buffer[50];
+	int largo;
+
+	printf(" \n Ingrese el nombre de %s \n ",variable);
+	scanf("%s",buffer);
+	largo=strlen(buffer);
+	nombre=(char*)malloc(largo*sizeof(char));
+	strcpy(nombre,buffer);
+
+
+	return nombre;
+}
+
+int ingresarPrecio(){ // devuelve un Int que se ingresa
+	int precio;
+
+	printf(" \nIngrese el precio  \n");
+	scanf("%d",&precio);
+
+	return precio;
+}
