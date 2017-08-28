94d27b5af7a5dce9b630c64dcf5452fb27d5c34a
ElPera <31422293+ElPera@users.noreply.github.com>
2017-08-28 19:38:51 -0300

    Resto Funciones Crear
    struct Producto * crearProducto (int codigoIngreasdo, char *nombreIngresado, int precioIngreado);
    struct NodoBoleta * crearNodoBoleta (char *numeroBoleta);
    struct VentasEnDia * crearVentasEnDia (int dia, int mes, int ano);
    struct NodoVentasEnDia * crearNodoVentasEnDia ();
    struct Surcursal * crearSucursal (char *direccionIngresada, char * administradorIngresado, char *rutAdministradoringresado);
    struct Promocion * crearPromocion (int codigoIngresado, int precioIngresado);

--- ProyectoLomitoAleman/lomitoAleman.c
+++ ProyectoLomitoAleman/lomitoAleman.c
@@ -68,10 +68,26 @@ main(){
 
 }
 
+struct Producto * crearProducto (int codigoIngreasdo, char *nombreIngresado, int precioIngreado){
+	struct Producto *nuevo;
+	
+	nuevo = (struct Producto * ) malloc (sizeof(struct Producto));
+	
+	nuevo -> nombre = (char *) malloc (sizeof(char) * strlen(nombreIngreado));
+	strcpy(nuevo -> nombre, nombreIngresado);
+	
+	nuevo -> codigo = codigoIngresado;
+	nuevo -> precio = precioIngresado;
+	
+	return nuevo;
+}
+
 struct NodoBoleta * crearNodoBoleta (char *numeroBoleta){
 	struct NodoBoleta *nuevo;
 
 	nuevo = (struct NodoBoleta*) malloc (sizeof(struct NodoBoleta));
+	
+	//Asignar Memoria a **productos??
 
 	nuevo -> sig = NULL;
 
@@ -91,83 +107,50 @@ struct VentasEnDia * crearVentasEnDia (int dia, int mes, int ano){
 
 }
 
-
-struct Producto *modificarProducto(int codigoIngresado, char *nombreIngresado, int precioIngresado)
-{
-	struct Producto *modificaciones;
-	int largo;
-
-	modificaciones=((struct Producto*)malloc(sizeof(struct Producto)));
-
-	largo=strlen(nombreIngresado);
-	modificaciones->nombre=(char*)malloc(largo*sizeof(char));
-	strcpy(modificaciones->nombre,nombreIngresado);
-
-	modificaciones->codigo=codigoIngresado;
-	modificaciones->precio=precioIngresado;
-
-	return modificaciones;
+struct NodoVentasEnDia * crearNodoVentasEnDia (){
+	struct NodoVentasEnDia *nuevo;
+	
+	nuevo = (struct NodoVentasEnDia*) malloc ( sizeof(struct NodoVentaEnDia));
+	
+	nuevo -> datos = NULL;
+	nuevo -> sig = NULL;
+	nuevo -> ant = NULL;
+	
+	return nuevo;
 }
 
-struct Sucursal *modificarSucursal(char *direccionIngresada, char *administradorIngresada, char *rutAdministradorIngresada)
-{
-	struct Sucursal *modificaciones;
-	int largo;
-
-	modificaciones=((struct Sucursal*)malloc(sizeof(struct Sucursal)));
-
-	largo=strlen(direccionIngresada);
-	modificaciones->direccion=(char*)malloc(largo*sizeof(char));
-	strcpy(modificaciones->direccion,direccionIngresada);
-
-	largo=strlen(administradorIngresada);
-	modificaciones->administrador=(char*)malloc(largo*sizeof(char));
-	strcpy(modificaciones->administrador,administradorIngresada);
-
-	largo=strlen(rutAdministradorIngresada);
-	modificaciones->rutAdministrador=(char*)malloc(largo*sizeof(char));
-	strcpy(modificaciones->rutAdministrador,rutAdministradorIngresada);
-
-
-	return modificaciones;
+struct Surcursal * crearSucursal (char *direccionIngresada, char * administradorIngresado, char *rutAdministradoringresado){
+	struct Sucursal *nuevo;
+	
+	nuevo = (struct Sucursal*) malloc (sizeof(struct Sucursal));
+	
+	nuevo -> direccion = (char *) malloc (sizeof(char) * strlen(direccionIngresada));
+	strcpy(nuevo -> direccion, direccionIngresada);
+	
+	nuevo -> administrador = (char *) malloc (sizeof(char) * strlen(administradorIngresado));
+	strcpy(nuevo -> administrador, administradorIngresado);
+	
+	nuevo -> rutAdministrador = (char *) malloc (sizeof(char) * strlen(rutAdministradorIngresado));
+	strcpy(nuevo -> rutAdministrador, rutAdministradorIngresado);
+	
+	//Asignar Memoria a *promociones[]??
+	//Asignar Memoria a *productos[]??
+	
+	nuevo -> ventas = NULL;
+	
+	return NULL;
 }
 
-struct LomitoAleman *modificarDueno(char *nombreIngresado, struct Sucursal *sucursalesActuales)
-{
-	struct LomitoAleman *modificaciones;
-	int largo;
-
-	largo=strlen(nombreIngresado);
-	modificaciones->dueno=(char*)malloc(largo*sizeof(char));
-	strcpy(modificaciones->dueno,nombreIngresado);
-
-	modificaciones->sucursales=sucursalesActuales;
-
-	return modificaciones;
+struct Promocion * crearPromocion (int codigoIngresado, int precioIngresado){
+	struct Promocion *nuevo;
+	
+	nuevo = (struct Promocion*) malloc (sizeof(struct Promocion));
+	
+	//Asignar Memoria a **consumo??
+	//Asignar Memoria a **liquidos??
+	
+	nuevo -> codigo = codigoIngresado;
+	nuevo -> precio = precioIngresado;
+	
+	return nuevo;
 }
-
-char *ingresarChar(char *variable){ // devuelve un char que se ingresa
-									// ejmplo, ingrese el nombre de "dueno"   o ingrese el nombre de "un usuario"
-	char *nombre;
-	char buffer[50];
-	int largo;
-
-	printf(" \n Ingrese el nombre de %s \n ",variable);
-	scanf("%s",buffer);
-	largo=strlen(buffer);
-	nombre=(char*)malloc(largo*sizeof(char));
-	strcpy(nombre,buffer);
-
-
-	return nombre;
-}
-
-int ingresarPrecio(){ // devuelve un Int que se ingresa
-	int precio;
-
-	printf(" \nIngrese el precio  \n");
-	scanf("%d",&precio);
-
-	return precio;
-}
-
