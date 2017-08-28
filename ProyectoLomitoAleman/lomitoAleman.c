1e46130b93ae157bed23a4ba095985fdb04da68f
graninty <31422278+graninty@users.noreply.github.com>
2017-08-28 19:32:22 -0300

    Update lomitoAleman.c

--- ProyectoLomitoAleman/lomitoAleman.c
+++ ProyectoLomitoAleman/lomitoAleman.c
@@ -18,6 +18,7 @@ struct Sucursal{
 	struct Promocion *promociones[MAXPROMO]
 	struct NodoVentas *ventas;
 	struct Producto *productos [MAXPROD];
+	struct Sucursal *sig, *ant; 
 
 }
 
@@ -25,14 +26,13 @@ struct Promocion{
 
 	struct producto **consumo;
 	struct producto **liquido;
-	int codigo;
-	int precio;
+	struct producto *;
 
 }
 
 struct NodoVentas{
 
-	struct Venta *datos;
+	struct VentasEnDia *datos;
 	struct NodoVentas *sig,*ant;
 
 }
@@ -44,7 +44,7 @@ struct Producto{
 
 }
 
-struct Venta{
+struct VentasEnDia{
 
 	int dia;
 	int mes;
@@ -92,82 +92,4 @@ struct VentasEnDia * crearVentasEnDia (int dia, int mes, int ano){
 }
 
 
-struct Producto *modificarProducto(int codigoIngresado, char *nombreIngresado, int precioIngresado)
-{
-	struct Producto *modificaciones;
-	int largo;
-
-	modificaciones=((struct Producto*)malloc(sizeof(struct Producto)));
-
-	largo=strlen(nombreIngresado);
-	modificaciones->nombre=(char*)malloc(largo*sizeof(char));
-	strcpy(modificaciones->nombre,nombreIngresado);
-
-	modificaciones->codigo=codigoIngresado;
-	modificaciones->precio=precioIngresado;
-
-	return modificaciones;
-}
-
-struct Sucursal *modificarSucursal(char *direccionIngresada, char *administradorIngresada, char *rutAdministradorIngresada)
-{
-	struct Sucursal *modificaciones;
-	int largo;
-
-	modificaciones=((struct Sucursal*)malloc(sizeof(struct Sucursal)));
-
-	largo=strlen(direccionIngresada);
-	modificaciones->direccion=(char*)malloc(largo*sizeof(char));
-	strcpy(modificaciones->direccion,direccionIngresada);
-
-	largo=strlen(administradorIngresada);
-	modificaciones->administrador=(char*)malloc(largo*sizeof(char));
-	strcpy(modificaciones->administrador,administradorIngresada);
-
-	largo=strlen(rutAdministradorIngresada);
-	modificaciones->rutAdministrador=(char*)malloc(largo*sizeof(char));
-	strcpy(modificaciones->rutAdministrador,rutAdministradorIngresada);
-
-
-	return modificaciones;
-}
-
-struct LomitoAleman *modificarDueno(char *nombreIngresado, struct Sucursal *sucursalesActuales)
-{
-	struct LomitoAleman *modificaciones;
-	int largo;
-
-	largo=strlen(nombreIngresado);
-	modificaciones->dueno=(char*)malloc(largo*sizeof(char));
-	strcpy(modificaciones->dueno,nombreIngresado);
-
-	modificaciones->sucursales=sucursalesActuales;
-
-	return modificaciones;
-}
-
-char *ingresarChar(char *variable){ // devuelve un char que se ingresa
-									// ejmplo, ingrese el nombre de "dueno"   o ingrese el nombre de "un usuario"
-	char *nombre;
-	char buffer[50];
-	int largo;
-
-	printf(" \n Ingrese el nombre de %s \n ",variable);
-	scanf("%s",buffer);
-	largo=strlen(buffer);
-	nombre=(char*)malloc(largo*sizeof(char));
-	strcpy(nombre,buffer);
-
-
-	return nombre;
-}
-
-int ingresarPrecio(){ // devuelve un Int que se ingresa
-	int precio;
-
-	printf(" \nIngrese el precio  \n");
-	scanf("%d",&precio);
-
-	return precio;
-}
 
