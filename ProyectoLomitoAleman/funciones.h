0d58ac2bd15f879f3edc045792b78e0ad23ffe10
Sergio Mora <sergio.mora.12@sansano.usm.cl>
2017-12-16 22:54:53 -0300

    Update funciones.h

--- ProyectoLomitoAleman/funciones.h
+++ ProyectoLomitoAleman/funciones.h
@@ -1,3 +1,32 @@
+funciones.h
+DETALLES
+ACTIVIDAD
+funciones.h
+Información de uso compartido
+S
+Información general
+Tipo
+Texto
+Tamaño
+4 KB (4.290 bytes)
+Espacio usado
+4 KB (4.290 bytes)
+Ubicación
+lomito aleman
+Propietario
+yo
+Modificado
+17:51 por mí
+Abierto
+22:54 por mí
+Creado el
+17:51 con Google Drive Web
+Descripción
+Añadir descripción
+Descargar permisos
+Los lectores pueden descargar
+Maximizado Se ha anulado la selección de todos los elementos 
+
 #ifndef __FUNCIONES_H__
 #define __FUNCIONES_H__
 
@@ -6,73 +35,60 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAXPRODUCTOS 1000
-#define MAXPROMOCIONES 100
-
-
 
 //FIRMAS_______________________________________
 
 //Agregar----------------
-int agregarProducto (int codigo, char *nombre, int precio, struct Producto **productos);
-int agregarPromocion (int codigo, char *nombre, int precio, struct Producto **consumo, struct Producto **liquido, struct Sucursal *sucursal);
+int agregarProducto (int codigo, char *nombre, int precio, struct Sucursal *sucur);
 int agregarSucursal(struct Empresa* empresa, char *direccion, char *administrador, char *rutAdministrador, int id);
 int agregarBoleta(int numeroBoleta, struct Producto **consumo);
-
+int agregarProductoEnArbol(struct Producto *nuevo, struct NodoProductos *productos);
+	
 //Eliminar----------------
 int eliminarSucursal(struct Empresa *empresa,struct Sucursal *sucursal);    
 int eliminarProducto(int codigo, struct Sucursal *sucursal);
-int eliminarPromocion(int codigo, struct Sucursal *sucursal);
-void eliminarProductoDeConsumo (struct Promocion *promo, struct Producto *prod);
-void eliminarProductoDeLiquido (struct Promocion *promo, struct Producto *prod);
 void eliminarProductoArreglo (struct Producto **arreglo, struct Producto *prod);
 
 //Trabajo----------------
 int nuevaBoleta(int numeroBoleta, struct Producto **productos, struct Sucursal *sucursal);
 int resumenVentas(int diaBuscado, int mesBuscado, int anoBuscado, struct NodoVentas *ventas); //Dejalo asi, menos enredos
 int totalVendido (struct Ventas *ven);
-void abrirCerrarCaja(struct Sucursal *sucursal);
-void eliminarDatosPromocion (struct Promocion *promo);
-void ajustarPromocionesAsociadasAUnProducto (struct Promocion **promociones, struct Producto *prod);
-struct Producto ** ajustarArreglo (struct Producto **arreglo, struct Producto *prod);
 int anoBisiesto(int ano);
+int ordenamientoRecurcivo(struct NodoProductos *ndo);
+int equilibrarArbol(struct Sucursal *sucur);
+int listarArbol(struct NodoProductos *productos,struct NodoProductos **ultimo);
 void diaSiguiente(int *dia , int *mes, int *ano);
 void cerrarCaja(struct Sucursal *sucur);
 void abrirCaja(struct Sucursal *sucur);
+int calcularTotalBoleta(struct NodoBoleta *boleta);
 
 //Ingreso----------------
 int ingresarInt(const char *mensaje);
 char * ingresarChar(const char *mensaje);
-struct Producto ** crearArregloProductos(const char *mensaje, struct Sucursal *sucursal);
 
 //Modificar----------------
 int modificarProducto(int codigoIngresado, char *nombreIngresado, int precioIngresado, struct Sucursal *tienda); 
 struct Empresa * modificarDueno(char *nombreIngresado, struct Empresa *empresa); 
 struct Sucursal *modificarSucursal(char *direccionIngresada, char *administradorIngresada, char *rutAdministradorIngresada, struct Sucursal *sucursal);
-struct Producto * modificarPromocion(int codigo, char *nombre, int precio, struct Producto **consumo, struct Producto **liquido, struct Sucursal *sucursal);
 
 //Crear----------------
 struct Empresa * crearEmpresa(char *nombre,char *dueno); 
-struct NodoBoleta * crearNodoBoleta (int numBoleta, int totalComprado); 
+struct NodoBoleta * crearNodoBoleta (int numBoleta); 
 struct Ventas * crearVentas (int dia, int mes, int ano); 
 struct NodoVentas * crearNodoVentas (); 
 struct Sucursal * crearSucursal (char *direccionIngresada, char * administradorIngresado, char *rutAdministradorIngresado, int idIngresada); 
-struct Promocion * crearPromocion (struct Producto *product, struct Producto **consumos, struct Producto **liquidos); 
 struct Producto * crearProducto (int codigo, char *nombre, int precio); 
 
 //Buscar----------------
 struct Sucursal * buscarSucursal(struct Sucursal *head, int id);//retorna la sucursal buscada por ID
-struct Producto * buscarProducto (int codigo, struct Producto **productos); 
+struct NodoProductos * buscarProducto (int codigo, struct NodoProductos *productos);
 struct NodoBoleta * buscarBoletaEnBoletas (int numeroBoleta, struct NodoBoleta *boleta); 
 struct NodoBoleta * buscarBoletaEnVentas (int numeroBoleta, struct NodoVentas *ventas); 
-struct Producto * buscarConsumo (struct Producto *pConsumo, struct Producto **consumos); 
-struct Producto * buscarLiquido (struct Producto *pLiquido, struct Producto **liquidos); 
 struct NodoBoleta * buscarUltimaBoletaEnListaVentas (struct NodoVentas *head);
 struct NodoVentas * buscarUltimoNodoVentas (struct NodoVentas *head);
 
-
 //Verificar----------------
-struct Producto * verificarDosProductosParaPromocion (struct Producto *productoConsumo, struct Producto *productoLiquido, struct Promocion **promociones); 
+
 
 //Imprimir----------------
 void imprimirSucursales (struct Sucursal *head); 
@@ -93,24 +109,15 @@ struct Empresa{
 };
 
 struct Sucursal{
-    int id;
+    int id; 
     char *direccion;
     char *administrador;
     char *rutAdministrador;
-    struct Promocion *promociones[MAXPROMOCIONES];
-    struct Producto *productos [MAXPRODUCTOS];
-    struct NodoBoleta *ultimaBoleta;
+    struct NodoProductos *productos;    
     struct Ventas *cajaAbierta;
     struct NodoVentas *ventas;
+	struct NodoBoleta *ultimaBoleta;
     struct Sucursal *sig, *ant;
-
-};
-
-struct Promocion{
-
-    struct Producto **consumo;
-    struct Producto **liquido;
-    struct Producto *dato;
 
 };
 
@@ -119,6 +126,13 @@ struct NodoVentas{
     struct Ventas *datos;
     struct NodoVentas *sig,*ant;
 
+};
+
+struct NodoProductos{
+	
+	struct Producto *datos;
+	struct NodoProductos *der, *izq;
+	
 };
 
 struct Producto{
@@ -142,8 +156,8 @@ struct Ventas{
 struct NodoBoleta{
 
     int numeroBoleta;
-//  struct Producto **productos; 
-    int total;
+	int nPV; //numero de productos vendidos
+    struct Producto **productos; 
     struct NodoBoleta *sig;
 
 };
