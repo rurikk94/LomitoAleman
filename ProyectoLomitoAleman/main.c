769e79e6cd551c4b8cdeaf8dad919a3b106609ae
Sergio Mora <sergio.mora.12@sansano.usm.cl>
2017-12-16 22:55:11 -0300

    Update main.c

--- ProyectoLomitoAleman/main.c
+++ ProyectoLomitoAleman/main.c
@@ -1,3 +1,32 @@
+main.c
+DETALLES
+ACTIVIDAD
+main.c
+Informaci贸n de uso compartido
+S
+Informaci贸n general
+Tipo
+Texto
+Tama帽o
+10 KB (10.389 bytes)
+Espacio usado
+10 KB (10.389 bytes)
+Ubicaci贸n
+lomito aleman
+Propietario
+yo
+Modificado
+17:51 por m铆
+Abierto
+17:51 por m铆
+Creado el
+17:51 con Google Drive Web
+Descripci贸n
+A帽adir descripci贸n
+Descargar permisos
+Los lectores pueden descargar
+Maximizado Se ha anulado la selecci贸n de todos los elementos 
+
 #include "funciones.h"
 
 int main (){
@@ -25,12 +54,10 @@ int main (){
             printf("\n\nRegistros:\n\n");
             printf("1.-Registrar sucursal\n");
             printf("2.-Registrar producto\n");
-            printf("3.-Registrar promocion\n");
             printf("\n\nEdiciones:\n\n");
-            printf("4.-Editar sucursal\n");
-            printf("5.-Editar producto\n");
-            printf("6.-Editar promocion\n");
-            printf("7.-Ventas\n\n");
+            printf("3.-Editar sucursal\n");
+            printf("4.-Editar producto\n");
+            printf("5.-Ventas\n\n");
             printf("0.-Salir\n");
             printf("\nOpcion: ");
             scanf("%d",&opcion);
@@ -54,7 +81,10 @@ int main (){
                     imprimirSucursales(empresa->sucursales);
                     printf("\n\n ingrese ID: ");
                     scanf ("%d",&sucursalId);
-                    respuesta=agregarProducto(ingresarInt("ingrese codigo del producto"),ingresarChar("ingrese nombre producto"), ingresarInt("ingrese precio"),buscarSucursal(empresa->sucursales,sucursalId)->productos);//ingresa producto en la sucursal
+					if(buscarSucursal(empresa->sucursales,sucursalId)==NULL){
+						break;
+					}
+                    respuesta=agregarProducto(ingresarInt("ingrese codigo del producto"),ingresarChar("ingrese nombre producto"), ingresarInt("ingrese precio"),buscarSucursal(empresa->sucursales,sucursalId));//ingresa producto en la sucursal
                     if(respuesta==0){
                         printf("Error en registro");
                     }
@@ -63,20 +93,6 @@ int main (){
                     }
                     break;
                 case 3:
-                    printf("\n\nSeleccione Sucursal:\n\n");
-                    imprimirSucursales(empresa->sucursales);
-                    printf("\n\n ingrese ID: ");
-                    scanf ("%d",&sucursalId);
-                    sucursal=buscarSucursal(empresa->sucursales,sucursalId);
-                    respuesta=agregarPromocion(ingresarInt("ingrese codigo de promocion"), ingresarChar("ingrese nombre promocion"), ingresarInt("ingrese precio"), crearArregloProductos("consumo",sucursal), crearArregloProductos("liquido",sucursal), sucursal);//ingresa promocion en la sucursal
-                    if(respuesta==0){
-                        printf("Error en registro");
-                    }
-                    if(respuesta==1){
-                        printf("registo completado");
-                    }
-                    break;
-                case 4:
                     do{
                         printf("\n\nSeleccione Sucursal:\n\n");
                         imprimirSucursales (empresa -> sucursales);
@@ -116,7 +132,7 @@ int main (){
                     }while(opcion);//!=0 
                     opcion=1;//para no cerrar el programa
                     break;
-                case 5:
+                case 4:
                     do{
                         printf("\n\nSeleccione Sucursal:\n\n");
                         imprimirSucursales(empresa->sucursales);
@@ -146,16 +162,16 @@ int main (){
                                 }
                                 break;
                             case 2:
-                                producto=sucursal->productos[ingresarInt("ingrese codigo del producto")];
+								producto=buscarProducto (ingresarInt("ingrese codigo del producto"), sucursal->productos)->datos;
                                 modificarProducto(producto->codigo, producto->nombre, ingresarInt("ingrese nuevo precio"), sucursal);
                                 break;
                             case 3:
-                                producto=sucursal->productos[ingresarInt("ingrese codigo del producto")];
+                                producto=buscarProducto (ingresarInt("ingrese codigo del producto"), sucursal->productos)->datos;
                                 modificarProducto(producto->codigo, ingresarChar("ingrese nuevo nombre"), producto->precio, sucursal);
                                 break;
                             case 4:
-                                producto=sucursal->productos[ingresarInt("ingrese codigo del producto")];
-                                agregarProducto(ingresarInt("ingrese nuevo codigo del producto"), producto->nombre, producto->precio, sucursal->productos);
+                                producto=buscarProducto (ingresarInt("ingrese codigo del producto"), sucursal->productos)->datos;
+                                agregarProducto(ingresarInt("ingrese nuevo codigo del producto"), producto->nombre, producto->precio, sucursal);
                                 eliminarProducto(producto->codigo, sucursal);
                                 break;
                             default:
@@ -164,55 +180,7 @@ int main (){
                     }while(opcion);//!=0
                     opcion=1;//para no cerrar el programa
                     break;
-                case 6:
-                    do{
-                        printf("\n\nSeleccione Sucursal:\n\n");
-                        imprimirSucursales(empresa->sucursales);
-                        printf("\n\n ingrese ID: ");
-                        scanf ("%d",&sucursalId);
-                        sucursal=buscarSucursal(empresa->sucursales,sucursalId);
-                        printf("\n\nUd Editara esta Sucursal:\n\n");
-                        mostrarSucursal(sucursal);
-                        printf("\n\nElija una opcion:\n\n");
-                        printf("1.-Eliminar Promocion\n");
-                        printf("2.-Cambiar precio\n");
-                        printf("3.-Cambiar nombre\n");
-                        printf("4.-Cambiar codigo\n");
-                        printf("0.-Regresar\n");
-                        printf("\nOpcion: ");
-                        scanf("%d",&opcion);
-                        switch(opcion){
-                            case 0:
-                                break;
-                            case 1:
-                                respuesta=eliminarPromocion(ingresarInt("ingrese codigo del producto"), sucursal);
-                                if(respuesta==0){
-                                printf("Error al Eliminar");
-                                }
-                                if(respuesta==1){
-                                printf("Eliminado correctamente");
-                                }
-                                break;
-                            case 2:
-                                producto=sucursal->productos[ingresarInt("ingrese codigo del producto")];
-                                modificarPromocion(producto->codigo, producto->nombre, ingresarInt("ingrese nuevo precio"), sucursal->promociones[producto->codigo]->consumo, sucursal->promociones[producto->codigo]->liquido, sucursal);
-                                break;
-                            case 3:
-                                producto=sucursal->productos[ingresarInt("ingrese codigo del producto")];
-                                modificarPromocion(producto->codigo, ingresarChar("ingrese nuevo nombre"), producto->precio, sucursal->promociones[producto->codigo]->consumo, sucursal->promociones[producto->codigo]->liquido, sucursal);
-                                break;
-                            case 4:
-                                producto=sucursal->productos[ingresarInt("ingrese codigo del producto")];
-                                agregarPromocion(ingresarInt("ingrese nuevo codigo de promocion"), producto->nombre, producto->precio, sucursal->promociones[producto->codigo]->consumo, sucursal->promociones[producto->codigo]->liquido, sucursal);
-                                eliminarPromocion(producto->codigo, sucursal);
-                                break;
-                            default:
-                                break;
-                        }                       
-                    }while(opcion);//!=0
-                    opcion=1;//para no cerrar el programa
-                    break;
-                case 7:
+                case 5:
                     do{
                         printf("\n\nSeleccione Sucursal:\n\n");
                         imprimirSucursales(empresa->sucursales);
@@ -241,7 +209,7 @@ int main (){
                                 abrirCerrarCaja(sucursal);
                                 break;
                             case 2:
-                                nuevaBoleta((sucursal->ultimaBoleta->numeroBoleta)+1, crearArregloProductos("consumo",sucursal),sucursal);//縠l "+1" se puede realizar aqui? 縠s mejor dentro de la funcion?
+                                nuevaBoleta((sucursal->ultimaBoleta->numeroBoleta)+1, crearArregloProductos("consumo",sucursal),sucursal);//偶el "+1" se puede realizar aqui? 偶es mejor dentro de la funcion?
                                 imprimirBoleta(sucursal->ultimaBoleta);
                                 break;
                             case 3:
